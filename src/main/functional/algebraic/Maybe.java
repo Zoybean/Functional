a789591a5d6e6e655978776167061ce2f6b63648
Zoey Llewellyn Hewll <zoeyhewll@gmail.com>
2018-04-18 15:50:22 +0800

    Add equals, hashCode, and unsafe operations

--- src/main/functional/algebraic/Maybe.java
+++ src/main/functional/algebraic/Maybe.java
@@ -19,245 +19,328 @@ package functional.algebraic;
 
 import com.sun.istack.internal.Nullable;
 import functional.throwing.ThrowingConsumer;
+import functional.throwing.ThrowingFunction;
+import functional.throwing.ThrowingRunnable;
 import functional.throwing.ThrowingSupplier;
 
+import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
 /**
- * <p>A container which may be empty, or hold a value; and provides pattern matching to safely unwrap the data.</p>
- *
- * <p>The type is sealed with a private constructor to ensure the only subclasses are {@link Nothing} and {@link Just}.</p>
+ * <p>A container which may be empty, or hold a value; and provides pattern matching to safely unwrap the data.
+ * <p>
+ * <p>The type is sealed with a private constructor to ensure the only subclasses are {@link Nothing} and {@link Just}.
  *
  * @param <V> The type of the optional value
  * @author Zoey Hewll
  */
 public abstract class Maybe<V> implements ThrowingSupplier<V, IllegalStateException>
 {
-  /**
-   * Private constructor to seal the type.
-   */
-  private Maybe() {}
-
-  /**
-   * Effectively final, singleton instance.
-   */
-  private static Nothing<?> NOTHING;
-
-  /**
-   * Perform pattern matching on the potential states of the container.
-   * @param some the operation to perform on the contained value if there is one
-   * @param none the operation to perform if there is no contained value
-   * @param <T> the type of the value returned by the supplied operations
-   * @return the result of the matched operation
-   */
-  public abstract <T> T match(Function<? super V, ? extends T> some, Supplier<? extends T> none);
-
-  /**
-   * Perform pattern matching on the potential states of the container.
-   * @param some the operation to perform on the contained value if there is one
-   * @param none the operation to perform if there is no contained value
-   */
-  public abstract void match(Consumer<? super V> some, Runnable none);
-
-  /**
-   * Performs the operation on any contained value and returns a Maybe of the result.
-   * @param f the function to apply to the contained value
-   * @param <T> the type of the function's return value
-   * @return the modified Maybe
-   */
-  public abstract <T> Maybe<T> map(Function<? super V, ? extends T> f);
-
-  /**
-   * Performs the operation on any contained value and returns the resulting Maybe if there is one.
-   * @param f the function to apply to the contained value
-   * @param <T> the type of the function's optional return value
-   * @return the modified Maybe
-   */
-  public abstract <T> Maybe<T> bind(Function<? super V, ? extends Maybe<? extends T>> f);
-
-  /**
-   * Returns the contained value, if it exists, otherwise raise an exception.
-   * @return The contained value, if it exists
-   * @throws IllegalStateException if nothing is contained
-   */
-  public abstract V get() throws IllegalStateException;
-
-  /**
-   * Returns the contained value, or the supplied default if it does not exits.
-   * @param value the default value to use
-   * @return the contained value, or the supplied default if it does not exits
-   */
-  public V fromMaybe(V value)
-  {
-    return match(
-        (V v) -> v,
-        ()    -> value
-    );
-  }
-
-  /**
-   * Returns true if there is a contained value.
-   * @return true if there is a contained value
-   */
-  public boolean isJust()
-  {
-    return match(
-        (V v) -> true,
-        ()    -> false
-    );
-  }
-
-  /**
-   * Converts a nullable value into an equivalent Maybe, returning Nothing if the value is null, and just(value) otherwise.
-   * @param value the value to wrap
-   * @return nothing() if the value is null, and just(value) otherwise
-   */
-  public Maybe<V> of(@Nullable V value)
-  {
-    return value == null
-        ? nothing()
-        : just(value);
-  }
-
-  /**
-   * Turns an Optional into a Maybe, turning Empty into Nothing, and a present value into just(value).
-   * @param value the optional to transform
-   * @return A Maybe mirroring the Optional parameter
-   */
-  public Maybe<V> of(Optional<V> value)
-  {
-    return value.map(Maybe::just).orElse(nothing());
-  }
-
-  /**
-   * Wraps the value in a Maybe.
-   * @param value The value to wrap
-   * @param <V> The type of the containted value
-   * @return The value wrapped in a Maybe
-   */
-  public static <V> Maybe<V> just(V value)
-  {
-    return new Just<>(value);
-  }
-
-  /**
-   * Returns the singleton Nothing instance.
-   * @param <V> The type of the non-existent contained value
-   * @return The singleton Nothing instance
-   */
-  public static <V> Maybe<V> nothing()
-  {
-    if (NOTHING == null)
-    {
-       NOTHING = new Nothing<>();
-    }
-    @SuppressWarnings("unchecked") Nothing<V> nothing = (Nothing<V>) NOTHING;
-    return nothing;
-  }
-
-  /**
-   * Upcast the container by upcasting the contained type.
-   * @param m The Maybe to cast
-   * @param <T> The type to cast to
-   * @param <V> The type to cast from
-   * @return A Maybe of the upcast type
-   */
-  private static <T, V extends T> Maybe<T> cast(Maybe<V> m)
-  {
-    return m.match(
-        (T value) -> just(value),
-        ()        -> nothing()
-    );
-  }
-
-  /**
-   * The class representing an absent value.
-   * @param <V> unused
-   */
-  private static class Nothing<V> extends Maybe<V>
-  {
-    @Override
-    public <T> T match(Function<? super V, ? extends T> some, Supplier<? extends T> none)
-    {
-      return none.get();
-    }
-
-    @Override
-    public void match(Consumer<? super V> some, Runnable none)
-    {
-      none.run();
-    }
-
-    @Override
-    public <T> Maybe<T> map(Function<? super V, ? extends T> f)
-    {
-      return nothing();
-    }
-
-    @Override
-    public <T> Maybe<T> bind(Function<? super V, ? extends Maybe<? extends T>> f)
-    {
-      return nothing();
-    }
-
-    @Override
-    public V get() throws IllegalStateException
-    {
-      throw new IllegalStateException();
-    }
-  }
-
-  /**
-   * The class representing a present value.
-   * @param <V> The type of the contained value.
-   */
-  private static class Just<V> extends Maybe<V>
-  {
     /**
-     * The contained value.
+     * Effectively final, singleton instance.
      */
-    final V value;
+    private static Nothing<?> NOTHING;
 
     /**
-     * Construct a Maybe with the provided value.
-     * @param value The value to wrap.
+     * Private constructor to seal the type.
      */
-    Just(V value)
-    {
-      this.value = value;
-    }
+    private Maybe() {}
 
-    @Override
+    /**
+     * Perform pattern matching on the potential states of the container.
+     *
+     * @param some the operation to perform on the contained value if there is one
+     * @param none the operation to perform if there is no contained value
+     * @param <T>  the type of the value returned by the supplied operations
+     * @return the result of the matched operation
+     * @throws E the exception thrown by the supplied operations
+     */
+    public abstract <T, E extends Exception> T unsafeMatch(ThrowingFunction<? super V, ? extends T, ? extends E> some, ThrowingSupplier<? extends T, ? extends E> none) throws E;
+
+    /**
+     * Perform pattern matching on the potential states of the container.
+     *
+     * @param some the operation to perform on the contained value if there is one
+     * @param none the operation to perform if there is no contained value
+     * @throws E the exception thrown by the supplied operations
+     */
+    public abstract <E extends Exception> void unsafeMatch(ThrowingConsumer<? super V, ? extends E> some, ThrowingRunnable<? extends E> none) throws E;
+
+    /**
+     * Perform pattern matching on the potential states of the container.
+     *
+     * @param some the operation to perform on the contained value if there is one
+     * @param none the operation to perform if there is no contained value
+     * @param <T>  the type of the value returned by the supplied operations
+     * @return the result of the matched operation
+     */
     public <T> T match(Function<? super V, ? extends T> some, Supplier<? extends T> none)
     {
-      return some.apply(value);
+        return unsafeMatch(
+                some::apply,
+                none::get);
     }
 
-    @Override
+    /**
+     * Perform pattern matching on the potential states of the container.
+     *
+     * @param some the operation to perform on the contained value if there is one
+     * @param none the operation to perform if there is no contained value
+     */
     public void match(Consumer<? super V> some, Runnable none)
     {
-      some.accept(value);
+        unsafeMatch(
+                some::accept,
+                none::run);
+    }
+
+    /**
+     * Performs the operation on any contained value and returns a Maybe of the result.
+     *
+     * @param f   the function to apply to the contained value
+     * @param <T> the type of the function's return value
+     * @return the modified Maybe
+     */
+    public abstract <T> Maybe<T> map(Function<? super V, ? extends T> f);
+
+    /**
+     * Performs the operation on any contained value and returns the resulting Maybe if there is one.
+     *
+     * @param f   the function to apply to the contained value
+     * @param <T> the type of the function's optional return value
+     * @return the modified Maybe
+     */
+    public abstract <T> Maybe<T> bind(Function<? super V, ? extends Maybe<? extends T>> f);
+
+    /**
+     * Returns the contained value, if it exists, otherwise raise an exception.
+     *
+     * @return The contained value, if it exists
+     * @throws IllegalStateException if nothing is contained
+     */
+    public abstract V get() throws IllegalStateException;
+
+    /**
+     * Returns the contained value, or the supplied default if it does not exits.
+     *
+     * @param value the default value to use
+     * @return the contained value, or the supplied default if it does not exits
+     */
+    public V fromMaybe(V value)
+    {
+        return match(
+                (V v) -> v,
+                () -> value
+        );
+    }
+
+    /**
+     * Returns true if there is a contained value.
+     *
+     * @return true if there is a contained value
+     */
+    public boolean isJust()
+    {
+        return match(
+                (V v) -> true,
+                () -> false
+        );
+    }
+
+    /**
+     * Converts a nullable value into an equivalent Maybe, returning Nothing if the value is null, and just(value) otherwise.
+     *
+     * @param value the value to wrap
+     * @return nothing() if the value is null, and just(value) otherwise
+     */
+    public Maybe<V> of(@Nullable V value)
+    {
+        return value == null
+                ? nothing()
+                : just(value);
+    }
+
+    /**
+     * Turns an Optional into a Maybe, turning Empty into Nothing, and a present value into just(value).
+     *
+     * @param value the optional to transform
+     * @return A Maybe mirroring the Optional parameter
+     */
+    public Maybe<V> of(Optional<V> value)
+    {
+        return value.map(Maybe::just).orElse(nothing());
+    }
+
+    /**
+     * Wraps the value in a Maybe.
+     *
+     * @param value The value to wrap
+     * @param <V>   The type of the containted value
+     * @return The value wrapped in a Maybe
+     */
+    public static <V> Maybe<V> just(V value)
+    {
+        return new Just<>(value);
+    }
+
+    /**
+     * Returns the singleton Nothing instance.
+     *
+     * @param <V> The type of the non-existent contained value
+     * @return The singleton Nothing instance
+     */
+    public static <V> Maybe<V> nothing()
+    {
+        if (NOTHING == null)
+        {
+            NOTHING = new Nothing<>();
+        }
+        @SuppressWarnings("unchecked") Nothing<V> nothing = (Nothing<V>) NOTHING;
+        return nothing;
+    }
+
+    /**
+     * Upcast the container by upcasting the contained type.
+     *
+     * @param m   The Maybe to cast
+     * @param <T> The type to cast to
+     * @param <V> The type to cast from
+     * @return A Maybe of the upcast type
+     */
+    private static <T, V extends T> Maybe<T> cast(Maybe<V> m)
+    {
+        return m.match(
+                (T value) -> just(value),
+                () -> nothing()
+        );
     }
 
     @Override
-    public <T> Maybe<T> map(Function<? super V, ? extends T> f)
-    {
-      return just(f.apply(value));
-    }
+    public abstract boolean equals(Object o);
 
     @Override
-    public <T> Maybe<T> bind(Function<? super V, ? extends Maybe<? extends T>> f)
+    public abstract int hashCode();
+
+    /**
+     * The class representing an absent value.
+     *
+     * @param <V> unused
+     */
+    private static class Nothing<V> extends Maybe<V>
     {
-      return cast(f.apply(value));
+        @Override
+        public boolean equals(Object o)
+        {
+            return o instanceof Nothing;
+        }
+
+        @Override
+        public int hashCode()
+        {
+            return 0;
+        }
+
+        @Override
+        public <T, E extends Exception> T unsafeMatch(ThrowingFunction<? super V, ? extends T, ? extends E> some, ThrowingSupplier<? extends T, ? extends E> none) throws E
+        {
+            return none.get();
+        }
+
+        @Override
+        public <E extends Exception> void unsafeMatch(ThrowingConsumer<? super V, ? extends E> some, ThrowingRunnable<? extends E> none) throws E
+        {
+            none.run();
+        }
+
+        @Override
+        public <T> Maybe<T> map(Function<? super V, ? extends T> f)
+        {
+            return nothing();
+        }
+
+        @Override
+        public <T> Maybe<T> bind(Function<? super V, ? extends Maybe<? extends T>> f)
+        {
+            return nothing();
+        }
+
+        @Override
+        public V get() throws IllegalStateException
+        {
+            throw new IllegalStateException();
+        }
     }
 
-    @Override
-    public V get()
+    /**
+     * The class representing a present value.
+     *
+     * @param <V> The type of the contained value.
+     */
+    private static class Just<V> extends Maybe<V>
     {
-      return value;
+        /**
+         * The contained value.
+         */
+        final V value;
+
+        /**
+         * Construct a Maybe with the provided value.
+         *
+         * @param value The value to wrap.
+         */
+        Just(V value)
+        {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object o)
+        {
+            if (o instanceof Just)
+            {
+                Just j = (Just) o;
+                return value.equals(j.value);
+            }
+            return false;
+        }
+
+        @Override
+        public int hashCode()
+        {
+            return Objects.hashCode(value);
+        }
+
+        @Override
+        public <T, E extends Exception> T unsafeMatch(ThrowingFunction<? super V, ? extends T, ? extends E> some, ThrowingSupplier<? extends T, ? extends E> none) throws E
+        {
+            return some.apply(value);
+        }
+
+        @Override
+        public <E extends Exception> void unsafeMatch(ThrowingConsumer<? super V, ? extends E> some, ThrowingRunnable<? extends E> none) throws E
+        {
+            some.accept(value);
+        }
+
+        @Override
+        public <T> Maybe<T> map(Function<? super V, ? extends T> f)
+        {
+            return just(f.apply(value));
+        }
+
+        @Override
+        public <T> Maybe<T> bind(Function<? super V, ? extends Maybe<? extends T>> f)
+        {
+            return cast(f.apply(value));
+        }
+
+        @Override
+        public V get()
+        {
+            return value;
+        }
     }
-  }
 }
--- src/main/functional/algebraic/Unit.java
+++ src/main/functional/algebraic/Unit.java
@@ -17,21 +17,90 @@
 
 package functional.algebraic;
 
+import functional.throwing.ThrowingConsumer;
+import functional.throwing.ThrowingFunction;
+import functional.throwing.ThrowingRunnable;
+import functional.throwing.ThrowingSupplier;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
 /**
  * A class representing the unit type.
- * This class has a single value.
+ * This class has a single value, and is used as a placeholder for data that would usually be present,
+ * but has been omitted.
+ * Returning Unit is equivalent to returning void, but not Void.
  *
  * @author Zoey Hewll
  */
 public final class Unit
 {
-  /**
-   * The single value of the Unit type.
-   */
-  public static final Unit unit = new Unit();
+    /**
+     * The single value of the Unit type.
+     */
+    public static final Unit unit = new Unit();
 
-  /**
-   * The only constructor for the Unit type.
-   */
-  private Unit() {}
+    /**
+     * The only constructor for the Unit type.
+     */
+    private Unit() {}
+
+    /**
+     * Turns a function returning void into a function returning Unit.
+     *
+     * @param f   The function to convert
+     * @param <E> The parameter type of the function
+     * @return An equivalent function that returns Unit instead of void
+     */
+    public static <E> Function<E, Unit> convert(Consumer<E> f)
+    {
+        return convertUnsafe(f::accept)::apply;
+    }
+
+    /**
+     * Turns a function returning void into a function returning Unit.
+     *
+     * @param f The function to convert
+     * @return An equivalent function that returns Unit instead of void
+     */
+    public static Supplier<Unit> convert(Runnable f)
+    {
+        return convertUnsafe(f::run)::get;
+    }
+
+    /**
+     * Turns a function returning void into a function returning Unit.
+     *
+     * @param f   The function to convert
+     * @param <T> The parameter type of the function
+     * @return An equivalent function that returns Unit instead of void
+     */
+    public static <T, E extends Exception> ThrowingFunction<T, Unit, E> convertUnsafe(ThrowingConsumer<T, E> f)
+    {
+        return f.andThen(() -> unit);
+    }
+
+    /**
+     * Turns a function returning void into a function returning Unit.
+     *
+     * @param f The function to convert
+     * @return An equivalent function that returns Unit instead of void
+     */
+    public static <E extends Exception> ThrowingSupplier<Unit, E> convertUnsafe(ThrowingRunnable<E> f)
+    {
+        return f.andThen(() -> unit);
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        return obj instanceof Unit;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return 0;
+    }
 }
