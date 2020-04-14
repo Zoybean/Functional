eccfd7a33b73e0d67f26bcd82e585fefdb2b212e
Zoey Llewellyn Hewll <zoeyhewll@gmail.com>
2020-04-14 22:43:43 +0800

    Update and document combinator classes

--- src/main/java/functional/combinator/Combinators.java
+++ src/main/java/functional/combinator/Combinators.java
@@ -21,41 +21,100 @@ import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
+/**
+ * Utility class with common combinators.
+ * @author Zoey Hewll
+ */
 public final class Combinators
 {
     /**
-     * Prevent construction of this class.
+     * Prevents construction of this class.
      */
     private Combinators(){}
 
+    /**
+     * Returns the argument.
+     * @param a   the value to return
+     * @param <A> the type of the value to return
+     * @return the argument.
+     */
     public static <A> A id(A a)
     {
         return a;
     }
 
+    /**
+     * Returns the first argument and discards the second.
+     * @param a   the value to return
+     * @param <A> the type of the value to return
+     * @param __  the unused argument
+     * @param <B> the type of the unused argument
+     * @return the first argument.
+     */
     public static <A, B> A constant(A a, B __)
     {
         return a;
     }
 
+    /**
+     * Discards the argument. No-op.
+     * @param __  the unused argument
+     * @param <A> the type of the unused argument
+     */
     public static <A> void noop(A __) {}
+
+    /**
+     * Does nothing to nothing. No-op.
+     */
     public static void none() {}
 
+    /**
+     * Composes two functions, such that the second is applied to the result of the first.
+     * @param ab  a function from A to B
+     * @param bc  a function from B to C
+     * @param <A> the type of the input to the first function
+     * @param <B> the intermediate type
+     * @param <C> the type of the output from the second function
+     * @return a function that applies the two given functions in sequence
+     */
     public static <A, B, C> Function<A, C> compose(Function<A, B> ab, Function<B, C> bc)
     {
         return (A a) -> bc.apply(ab.apply(a));
     }
 
+    /**
+     * Reverses the parameters of a function that has two parameters.
+     * @param f   the function to modify
+     * @param <A> the type of the first argument
+     * @param <B> the type of the second argument
+     * @param <C> the type of the result
+     * @return A function that has its parameters reversed
+     */
     public static <A, B, C> BiFunction<B, A, C> flip(BiFunction<A, B, C> f)
     {
         return (B b, A a) -> f.apply(a, b);
     }
 
+    /**
+     * Modifies the function's type, such that the parameter type is more specific and/or the return type is less specific.
+     * @param f   the function to modify
+     * @param <A> the new parameter type, a subclass of the original parameter type
+     * @param <B> the new return type, a superclass of the original return type
+     * @return an equivalent function that has different bounds on its types
+     */
     public static <A, B> Function<A, B> cast(Function<? super A, ? extends B> f)
     {
         return f::apply;
     }
 
+    /**
+     * Throws the given Throwable.
+     * @param t   the throwable to throw
+     * @param <T> the type of the throwable
+     * @param <A> the unused return type
+     * @return never
+     * @throws T unconditionally
+     */
     public static <T extends Throwable, A> A toss(T t) throws T
     {
         throw t;
--- src/main/java/functional/combinator/Curried.java
+++ src/main/java/functional/combinator/Curried.java
@@ -19,55 +19,123 @@ package functional.combinator;
 
 import java.util.function.*;
 
+/**
+ * Utility class with curried and currying-related combinators.
+ * @author Zoey Hewll
+ */
 public final class Curried
 {
     /**
-     * Prevent construction of this class.
+     * Prevents construction of this class.
      */
     private Curried(){}
 
+    /**
+     * Curried form of Combinators::constant.
+     */
     public static <A, B> Function<B, A> constant(A a)
     {
-        return Curried.<A, B, A>curry(Combinators::<A,B>constant).apply(a);
+        return Curried.<A, B, A>curry(Combinators::constant).apply(a);
     }
 
+    /**
+     * Curried form of Combinators::compose.
+     */
     public static <A, B, C> Function<Function<B, C>, Function<A, C>> compose(Function<A, B> ab)
     {
-        return Curried.<Function<A, B>, Function<B, C>, Function<A, C>>curry(Combinators::<A,B,C>compose).apply(ab);
+        return Curried.<Function<A, B>, Function<B, C>, Function<A, C>>curry(Combinators::compose).apply(ab);
     }
 
+    /**
+     * Curries the result of {@link Combinators#flip}.
+     */
     public static <A, B, C> Function<B, Function<A, C>> flip(BiFunction<A, B, C> f)
     {
         return curry(Combinators.flip(f));
     }
 
+    /**
+     * Curried form of {@link Curried#flip(BiFunction)}.
+     */
     public static <A, B, C> Function<B, Function<A, C>> flip(Function<A, Function<B, C>> f)
     {
-        return (B b) -> (A a) -> f.apply(a).apply(b);
+        return flip(uncurry(f));
     }
 
+    /**
+     * Returns an equivalent function-to-function that takes one argument at a time.
+     * @param f   the function to split
+     * @param <A> the type of the first argument
+     * @param <B> the type of the second argument
+     * @param <C> the eventual return type
+     * @return an equivalent function that takes one argument at a time
+     */
     public static <A, B, C> Function<A, Function<B, C>> curry(BiFunction<A, B, C> f)
     {
         return (A a) -> (B b) -> f.apply(a, b);
     }
 
-    public static <A, B> Consumer<B> curryWith(BiConsumer<A, B> f, A a)
+    /**
+     * Returns an equivalent function with the first argument fixed.
+     * @param f   the function to partially apply
+     * @param a   the value of the argument to fix
+     * @param <A> the type of the first argument
+     * @param <B> the type of the second argument
+     * @param <C> the eventual return type
+     * @return an equivalent function that takes one fewer argument
+     */
+    public static <A, B, C> Function<B, C> curryWith(BiFunction<A, B, C> f, A a)
     {
-        return (B b) -> f.accept(a, b);
+        return curry(f).apply(a);
     }
 
-    public static <A, B> Function<A, Consumer<B>> curry(BiConsumer<A, B> f)
+    /**
+     * Returns an equivalent function that takes both arguments at once.
+     * @param f   the function to join
+     * @param <A> the type of the first argument
+     * @param <B> the type of the second argument
+     * @param <C> the eventual return type
+     * @return an equivalent function that takes both arguments at once
+     */
+    public static <A, B, C> BiFunction<A, B, C> uncurry(Function<A, Function<B, C>> f)
+    {
+        return (A a, B b) -> f.apply(a).apply(b);
+    }
+
+    /**
+     * Returns an equivalent function-to-consumer that takes one argument at a time.
+     * @param f   the consumer to split
+     * @param <A> the type of the first argument
+     * @param <B> the type of the second argument
+     * @return an equivalent consumer that takes one argument at a time
+     */
+    public static <A, B> Function<A, Consumer<B>> curryConsumer(BiConsumer<A, B> f)
     {
         return (A a) -> (B b) -> f.accept(a, b);
     }
 
-    public static <A, B, C> Function<B, C> curryWith(BiFunction<A, B, C> f, A a)
+    /**
+     * Returns an equivalent consumer with the first argument fixed.
+     * @param f   the function to partially apply
+     * @param a   the value of the argument to fix
+     * @param <A> the type of the first argument
+     * @param <B> the type of the second argument
+     * @return an equivalent consumer that takes one fewer argument
+     */
+    public static <A, B> Consumer<B> curryConsumerWith(BiConsumer<A, B> f, A a)
     {
-        return (B b) -> f.apply(a, b);
+        return curryConsumer(f).apply(a);
     }
 
-    public static <A, B, C> BiFunction<A, B, C> uncurry(Function<A, Function<B, C>> f)
+    /**
+     * Returns an equivalent consumer that takes both arguments at once.
+     * @param f   the consumer to join
+     * @param <A> the type of the first argument
+     * @param <B> the type of the second argument
+     * @return an equivalent consumer that takes both arguments at once
+     */
+    public static <A, B> BiConsumer<A, B> uncurryConsumer(Function<A, Consumer<B>> f)
     {
-        return (A a, B b) -> f.apply(a).apply(b);
+        return (A a, B b) -> f.apply(a).accept(b);
     }
 }
