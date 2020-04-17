6f576d14290cbc7f498ad55a1b657109b06fc18f
Zoey Llewellyn Hewll <zoeyhewll@gmail.com>
2020-04-17 13:20:03 +0800

    Update Copyright

--- src/main/java/functional/combinator/Combinators.java
+++ src/main/java/functional/combinator/Combinators.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Zoey Hewll
+ * Copyright 2019, 2020 Zoey Hewll
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
